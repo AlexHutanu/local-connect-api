2e257611cdf9328f7ca500523855b6f3353af356
AlexHutanu <valexhutanu@gmail.com>
2025-11-02 18:37:32 +0200

    add CRUD operations and routing for User

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { createUser } from '../services/userService';
+import { createUser, getAllUsers, getUserById, deleteUser, updateUser } from '../services/userService';
+import { Types } from 'mongoose';
 
 
 export default (router: express.Router) => {
@@ -12,4 +13,43 @@ export default (router: express.Router) => {
             error: result.error
         });
     })
+
+    router.get('/users', async (req, res) => {
+        const result = await getAllUsers();
+        res.status(result.status).json({
+            message: result.message,
+            users: result.users,
+            error: result.error
+        });
+    });
+
+    router.get('/users/:userId', async (req, res) => {
+        const userId: Types.ObjectId = new Types.ObjectId(req.params.userId);
+        const result = await getUserById(userId);
+        res.status(result.status).json({
+            message: result.message,
+            user: result.user,
+            error: result.error
+        });
+    });
+
+    router.delete('/users/:userId', async (req, res) => {
+        const userId: Types.ObjectId = new Types.ObjectId(req.params.userId);
+        const result = await deleteUser(userId);
+        res.status(result.status).json({
+            message: result.message,
+            error: result.error
+        });
+    });
+
+    router.put('/users/:userId', async (req, res) => {
+        const userId: Types.ObjectId = new Types.ObjectId(req.params.userId);
+        const userData = req.body;
+        const result = await updateUser(userId, userData);
+        res.status(result.status).json({
+            message: result.message,
+            user: result.user,
+            error: result.error
+        });
+    });
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import { Types } from "mongoose";
 import { UserModel } from "../models/userModel";
 
 export const createUser = async (userData: any) => {
@@ -18,19 +19,83 @@ export const createUser = async (userData: any) => {
     }
 }
 
-// export async function getUsers() {
-//     try {
-//         const users = await UserModel.find();
-//         return {
-//             status: 200,
-//             message: "Users retrieved successfully",
-//             users: users
-//         }
-//     } catch (error) {
-//         return {
-//             status: 500,
-//             message: "Error retrieving users",
-//             error: error
-//         }
-//     }
-// }
+export const getUserById = async (userId: Types.ObjectId) => {
+    try {
+        const user = await UserModel.findById(userId);
+        if (user) {
+            return {
+                status: 200,
+                message: "User retrieved successfully",
+                user: user
+            }
+        } else {
+            return {
+                status: 404,
+                message: "User not found"
+            }
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: "Error retrieving user",
+            error: error
+        }
+    }
+}
+
+export const deleteUser = async (userId: Types.ObjectId) => {
+    try {
+        await UserModel.findByIdAndDelete(userId);
+        return {
+            status: 200,
+            message: "User deleted successfully"
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: "Error deleting user",
+            error: error
+        }
+    }
+}
+
+export const updateUser = async (userId: Types.ObjectId, userData: any) => {
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(userId, userData, { new: true });
+        if (updatedUser) {
+            return {
+                status: 200,
+                message: "User updated successfully",
+                user: updatedUser
+            }
+        } else {
+            return {
+                status: 404,
+                message: "User not found"
+            }
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: "Error updating user",
+            error: error
+        }
+    }
+}
+
+export const getAllUsers = async () => {
+    try {
+        const users = await UserModel.find();
+        return {
+            status: 200,
+            message: "Users retrieved successfully",
+            users: users
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: "Error retrieving users",
+            error: error
+        }
+    }
+}
