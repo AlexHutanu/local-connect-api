9703f85471e129c6232b1792bd00ee265a960da9
AlexHutanu <valexhutanu@gmail.com>
2025-11-05 15:41:09 +0200

    setup socket.io

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,31 @@ import router from "./router/index";
 import bodyParser from "body-parser";
 import compression from "compression";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("register", (userId) => {
+    socket.join(userId);
+    console.log(`User ${userId} joined their room`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 mongoose.Promise = Promise;
 
