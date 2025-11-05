27482e576dcda2668a912bf6fc8396b996493a48
AlexHutanu <valexhutanu@gmail.com>
2025-11-05 15:53:05 +0200

    add notification model

---
+++ src/models/notification.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface INotification extends Document {
+  userId: Types.ObjectId;
+  title: string;
+  message: string;
+  read: boolean;
+};
+
+const notificationSchema = new Schema<INotification>(
+    {
+        userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        title: { type: String, required: true },
+        message: { type: String, required: true },
+        read: { type: Boolean, default: false },
+    },
+    { timestamps: true }
+);
+
+export const Notification = model<INotification>("Notification", notificationSchema);
